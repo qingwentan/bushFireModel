728c0e657719f248d782bc9998b9e921ffba3ac1
aanchalb1201 <80521666+aanchalb1201@users.noreply.github.com>
2023-09-14 00:01:33 +1000

    Update app_visualisation.py

--- src/app_visualisation.py
+++ src/app_visualisation.py
@@ -133,6 +133,6 @@ def update_graph(selected_strategy, selected_input, selected_output):
     return fig
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+#if __name__ == '__main__':
+   # app.run_server(debug=True)
 
